c30cddc133d7befe4d6689af7056377270e05c54
AbhinavKr0110 <abhinavkr2345@gmail.com>
2025-12-22 18:06:30 +0530

    session page added

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -35,7 +35,11 @@ export async function createSession(req,res){
 }
 export async function getActiveSession(_,res){
     try {
-        const sessions = (await Session.find({status:"active"}).populate("host","name profileImage email clerkId")).sort({createdAt:-1}).limit(20);
+        const sessions = await Session.find({status:"active"})
+        .populate("host","name profileImage email clerkId")
+        .populate("participant","name profileImage email clerkId")
+        .sort({createdAt:-1})
+        .limit(20);
 
         res.status(200).json({sessions});
     } catch (error) {
